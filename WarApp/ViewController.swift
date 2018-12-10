32f70b5c936c7b02073a8c9cf78ccda474590378
Kalevyx <kalevyx@gmail.com>
2018-12-10 14:10:35 -0600

    initial submission!

---
+++ WarApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WarApp
+//
+//  Created by NN121local on 12/7/18.
+//  Copyright © 2018 Artra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
