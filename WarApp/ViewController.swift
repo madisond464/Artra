5236199ae57a4e78af27652b555bedb55abfb465
Kalevyx <kalevyx@gmail.com>
2018-12-11 14:15:28 -0600

    HandlingTips + IBOutlets and IBActions

--- WarApp/ViewController.swift
+++ WarApp/ViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var leftImageView: UIImageView!
+    
+    @IBOutlet weak var rightImageView: UIImageView!
+    
+    @IBOutlet weak var leftScoreLabel: UILabel!
+    
+    @IBOutlet weak var rightScoreLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
     }
-
-
+    
+    @IBAction func dealTapped(_ sender: Any) {
+        
+        let leftRandomNumber = arc4random_uniform(13) + 2
+        print("left random number is: \(leftRandomNumber)")
+        
+        let rightRandomeNumber = arc4random_uniform(13) + 2
+        print("right random number is: \(rightRandomeNumber)")
+        
+        leftImageView.image = UIImage(named: "card10")
+        
+        rightImageView.image = UIImage(named: "card13")
+        
+    }
+    
 }
 
